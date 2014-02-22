df6605c2dcd5996edc2026c81cdb7cabecd4031d
Phat Wangrungarun <phatograph@gmail.com>
2014-02-22 22:29:34 +0700

    Dependent destroy to project -> tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ActiveRecord::Base
 
   has_many :memberships, :dependent => :destroy
   has_many :users, :through => :memberships
-  has_many :tasks
+  has_many :tasks, :dependent => :destroy
 
   def tasks_count
     tasks.count
