fe68c4877c94e551741b7b0069e9801639be7568
Phat Wangrungarun <phatograph@gmail.com>
2014-02-27 00:16:58 +0700

    Membership -> Assignment dependency

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,7 +1,13 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+  has_many :tasks,
+    :through => :user
 
   validates_presence_of :user_id, :project_id
   validates_uniqueness_of :user_id, :scope => :project_id
+
+  after_destroy do |membership|
+    membership.user.tasks.destroy_all
+  end
 end
