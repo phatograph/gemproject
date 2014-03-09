267d725f2d3345f958204800398ec6ca5504a72b
Phat Wangrungarun <phatograph@gmail.com>
2014-03-09 22:57:09 +0700

    Add report mode

--- app/assets/javascripts/controllers/report_index_ctrl.js.coffee
+++ app/assets/javascripts/controllers/report_index_ctrl.js.coffee
@@ -2,9 +2,11 @@ angular.module('app.controllers').controller 'ReportIndexCtrl', [
   '$scope', '$route', '$location', '$filter', 'Project'
   ($scope,   $route,   $location,   $filter,   Project) ->
     Project.byRequesterDepartment(
+      mode: $route.current.params.mode
       startedAtStart: $route.current.params.startedAtStart
       startedAtEnd: $route.current.params.startedAtEnd
     ).then (data) ->
+      $scope.mode = data.mode
       $scope.startedAtStart = data.startedAtStart
       $scope.startedAtEnd = data.startedAtEnd
       $scope.departments = data.departments
@@ -15,8 +17,13 @@ angular.module('app.controllers').controller 'ReportIndexCtrl', [
     $scope.setStartedAtEnd = (time) ->
       $scope.setTime new Date($scope.startedAtStart), time
 
+    $scope.setMode = (mode) ->
+      $scope.mode = mode
+      $scope.setTime new Date($scope.startedAtStart), new Date($scope.startedAtEnd)
+
     $scope.setTime = (start, end) ->
       $location.search
+        mode: $scope.mode
         startedAtStart: +(start / 1000)
         startedAtEnd: +(end / 1000)
 ]
--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -5,20 +5,29 @@ class Api::ProjectsController < Api::BaseController
 
   def by_requester_department
     started_at_start = begin
-                         DateTime.strptime params[:started_at_start], '%s'
-                       rescue
-                         DateTime.now.beginning_of_week
-                       end
+      DateTime.strptime params[:started_at_start], '%s'
+    rescue
+      DateTime.now.beginning_of_week
+    end
 
-    started_at_end   = begin
-                         DateTime.strptime params[:started_at_end], '%s'
-                       rescue
-                         DateTime.now.end_of_week
-                       end
+    started_at_end = begin
+      DateTime.strptime params[:started_at_end], '%s'
+    rescue
+      DateTime.now.end_of_week
+    end
 
-    @projects  = @projects.where(:created_at => [started_at_start..started_at_end])
+    range = [started_at_start..started_at_end]
+    mode = params[:mode].present? ? params[:mode] : 'started'
+
+    query = case mode
+    when 'started' then { :created_at => range }
+    when 'ended'   then { :ended_at => range }
+    end
+
+    @projects = @projects.where(query)
 
     render :json => {
+      :mode => mode,
       :started_at_start => started_at_start,
       :started_at_end => started_at_end,
       :departments => @projects.group_by(&:requester_department)
